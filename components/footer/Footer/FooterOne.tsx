3b2db587c764bc5e4f96d4ca42bba9e5e0ebb01f
Pedro França <jppfeitosa@gmail.com>
2023-11-03 20:15:08 -0300

    feat: animated backgruond on Services page

--- components/footer/Footer/FooterOne.tsx
+++ components/footer/Footer/FooterOne.tsx
@@ -179,7 +179,7 @@ const navigation = {
             </div>
           </div>
           <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
-            <p className="text-xs leading-5 text-gray-500">&copy; 2023 RocketNey. All rights reserved.</p>
+            <p className="text-xs leading-5 text-gray-500">&copy; 2023 RocketNey. All reservados.</p>
           </div>
         </div>
       </footer>
---
+++ sections/Development/Header.tsx
@@ -0,0 +1,126 @@
+export default function Header() {
+  return (
+    <header class="relative isolate z-10 bg-[#F5F4F2]">
+      <nav
+        class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div class="flex lg:flex-1">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">RocketNey</span>
+            {/* <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt=""/> */}
+            <svg
+              id="Layer_1"
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 300 94.33"
+              class="h-8 w-auto"
+            >
+              <defs>
+                <clipPath id="clip-path">
+                  <path
+                    d="M52.41,12.33A37.34,37.34,0,1,1,15.07,49.67,37.33,37.33,0,0,1,52.41,12.33Z"
+                    fill="none"
+                    clip-rule="evenodd"
+                  />
+                </clipPath>
+              </defs>
+              <title>Logo Trilha Positivo</title>
+              <g clip-path="url(#clip-path)">
+                <path
+                  d="M44.51,45.09c-2.42-1.64-3.09-2.33-5.34-3.79C35.66,35.48,28.3,31.94,21,28.45c3.11-4.86,8.12-8.7,14-12,7.37,7.84,10.5,17.06,9.49,28.69Z"
+                  fill="#b2f0d9"
+                  stroke="#b2f0d9"
+                  stroke-miterlimit="22.93"
+                  stroke-width="0.88"
+                  fill-rule="evenodd"
+                />
+                <path
+                  d="M44.47,45.8c.66-10.38.1-17.55-9.51-29.23,6.33-3.8,13-4.14,19.59-4.14C57.54,21.64,60.87,44,89.69,42l1.07,10.76c-32.84,23.57-36.43-.1-46.29-6.93Z"
+                  fill="#ffac34"
+                  stroke="#ffac34"
+                  stroke-miterlimit="22.93"
+                  stroke-width="0.88"
+                  fill-rule="evenodd"
+                />
+                <path
+                  d="M39.17,41.3C42.24,50.55,25.73,62.81,19.5,68c24,35.78,68.78,18.72,71.52-15.6C59.22,74.94,51.13,46.92,39.17,41.3Z"
+                  fill="#0b5fcf"
+                  fill-rule="evenodd"
+                />
+                <path
+                  d="M54.07,12.43C60,33.3,69.18,43.57,89.2,42A37.9,37.9,0,0,0,54.07,12.43Z"
+                  fill="#ed6094"
+                  stroke="#ed6094"
+                  stroke-miterlimit="22.93"
+                  stroke-width="0.88"
+                  fill-rule="evenodd"
+                />
+                <path
+                  d="M21.33,27.69C10.27,44.31,15.88,62.07,19.5,68c27.72-18.61,26.07-29.49,1.83-40.34Z"
+                  fill="#fe0000"
+                  fill-rule="evenodd"
+                />
+              </g>
+              <path
+                d="M129.57,77.87v-3h-6.21a7.65,7.65,0,0,1-5.63-2.15,7.92,7.92,0,0,1-2-5.71V41.87h13.82v-3H115.75V28.13h-3.06V38.89h-4v3h4V66.71a11.4,11.4,0,0,0,2.89,8.18q2.82,3,8.44,3Zm148.82,0V58.42a20.1,20.1,0,0,0-5.55-14.48,17.69,17.69,0,0,0-13.41-5.8,17.88,17.88,0,0,0-13.57,5.8,20.44,20.44,0,0,0-5.38,14.48A20.18,20.18,0,0,0,246,72.91a17.74,17.74,0,0,0,13.41,5.79A16.42,16.42,0,0,0,266.88,77c.28-.16.76-.5,1.45-1s1.45-1.08,2.28-1.74,1.67-1.35,2.52-2.07l2.19-1.82v7.53Zm-3.07-11L273,69c-.87.77-1.77,1.53-2.68,2.27s-1.76,1.41-2.57,2-1.37,1-1.76,1.2a13.54,13.54,0,0,1-3,1,17.37,17.37,0,0,1-3.51.33,14.83,14.83,0,0,1-10.89-5,16.88,16.88,0,0,1-4.79-12.31,17.35,17.35,0,0,1,4.79-12.39,14.65,14.65,0,0,1,10.89-5A14.5,14.5,0,0,1,271,46.41a18,18,0,0,1,4.37,12.05v8.42Zm-43.61,11V54.12a15.56,15.56,0,0,0-15.9-16,15.65,15.65,0,0,0-7.11,1.58,16.29,16.29,0,0,0-5.38,4.46V20.26h-3.07V77.87h3.15V54.59A13.72,13.72,0,0,1,207,44.83a11.87,11.87,0,0,1,8.94-3.71,11.63,11.63,0,0,1,8.85,3.8,13.39,13.39,0,0,1,3.65,9.67V77.87Zm-40.39,0V20.26H188V77.87ZM179.64,27.8a2.2,2.2,0,0,0-.74-1.74,2.75,2.75,0,0,0-1.74-.66,2.07,2.07,0,0,0-1.74.74,2.14,2.14,0,0,0-.74,1.66,2.51,2.51,0,0,0,.74,1.82,2.37,2.37,0,0,0,1.74.66,2.48,2.48,0,0,0,1.82-.74,2.38,2.38,0,0,0,.66-1.74Zm-.74,50.07v-39h-3.31v39ZM166.07,27.8a2.23,2.23,0,0,0-.74-1.74,2.79,2.79,0,0,0-1.74-.66,2.07,2.07,0,0,0-1.74.74,2.11,2.11,0,0,0-.75,1.66,2.48,2.48,0,0,0,.75,1.82,2.35,2.35,0,0,0,1.74.66,2.5,2.5,0,0,0,1.82-.74,2.38,2.38,0,0,0,.66-1.74Zm-.74,50.07v-39H162v39Zm-10.43-36v-3h-3.64a12,12,0,0,0-7,2.32A9.88,9.88,0,0,0,141.53,44c-.74,1.13-1.39,2.22-1.94,3.27V38.89h-3.07v39h3.07V57.51a17.3,17.3,0,0,1,3.64-11.17q3.56-4.47,8.36-4.47Z"
+                fill-rule="evenodd"
+              />
+            </svg>
+          </a>
+          <p class="flex font-ClashGrotesk text-xs md:text-sm items-end ml-2">
+            Talentos & Performance
+          </p>
+        </div>
+        <div class="flex lg:hidden">
+          <button
+            type="button"
+            class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span class="sr-only">Open main menu</span>
+            <svg
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="hidden lg:flex lg:gap-x-12">
+          <a
+            href="http://www.triilha.com/"
+            class="text-sm font-medium font-ClashGrotesk text-black"
+          >
+            HOME
+          </a>
+          <a
+            href="http://www.triilha.com/services"
+            class="text-sm font-medium font-ClashGrotesk text-black"
+          >
+            SERVIÇOS
+          </a>
+          <a
+            href="http://www.triilha.com/partners"
+            class="text-sm font-medium font-ClashGrotesk text-black"
+          >
+            PARCERIAS
+          </a>
+          <a
+            href="http://www.triilha.com/contact"
+            class="text-sm font-medium font-ClashGrotesk text-black"
+          >
+            CONTATO
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
