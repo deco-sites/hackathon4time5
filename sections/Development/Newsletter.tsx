9029fecabd361e515b5ba36a290c4da962865785
Pedro França <jppfeitosa@gmail.com>
2023-11-04 06:26:19 -0300

    feat: Newsletter btn bg

--- sections/Development/Newsletter.tsx
+++ sections/Development/Newsletter.tsx
@@ -9,8 +9,8 @@ export default function Newsletter() {
           <form class="w-full max-w-md lg:col-span-5 lg:pt-2">
             <div class="flex gap-x-4">
               <label for="email-address" class="sr-only">Email address</label>
-              <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Your email" />
-              <button type="submit" class="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Subscribe</button>
+              <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-[#E0AC65] sm:text-sm sm:leading-6" placeholder="Your email" />
+              <button type="submit" class="flex-none rounded-md bg-[#E0AC65] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#E0AC65]">Subscribe</button>
             </div>
             <p class="mt-4 text-sm leading-6 text-gray-300">We care about your data. Read our <a href="#" class="font-semibold text-white">Privacy&nbsp;Policy</a>.</p>
           </form>
